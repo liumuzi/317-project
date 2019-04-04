76e977e53463c7cdf7df4cf36327d0f83ec96cb3
liumuzi <liumuzi1215@gmail.com>
2019-04-04 17:12:08 +0800

    2.1 (with minor bug)

--- src/pkg/Employee.java
+++ src/pkg/Employee.java
@@ -45,6 +45,9 @@ public class Employee implements User {
 			case 1:
 				showAvailablePositions(con);
 				break;
+			case 4:
+				//Go back
+				return 1;
 			default:
 				System.out.print("[Error] Invalid Operation Number!");
 				System.exit(0);
@@ -59,25 +62,55 @@ public class Employee implements User {
 	}
 	
 	void showAvailablePositions(Connection con) {
-		/* Require user information */
+		/* Require & check user information */
 		System.out.print("Please enter your ID:\n");
 		String id = scanner.nextLine();
+		//TODO check whether id exists
+		String chk_id = "SELECT * FROM Employee E WHERE E.Employee_ID = '" + id +"'";
+		try {
+			Statement chk_stmt = con.createStatement();
+			ResultSet chk_rs = chk_stmt.executeQuery(chk_id); 
+			if(!chk_rs.next())
+			{
+				System.out.print("Invalid Employee ID!\n");
+				return;
+			}
+			System.out.print("Valid ID! Porcessing......\n");
+		}catch (SQLException e){
+			e.printStackTrace();
+			System.exit(0);
+		}
 		
 		/* Database operation */
-		System.out.print("processing...");
-		String qry = "SELECT * "
-				+ "FROM Position P"
-				+ "WHERE P.Status = True and ";
+		String qry = "SELECT P.Position_ID, P.Position_Title, P.Salary, ER.Company, C.Size, C.Founded"
+				+ " FROM Position P, Employee EE, Employer ER, Company C"
+				+ " WHERE EE.Employee_ID = " + id
+				+ " AND P.Employer_ID = ER.Employer_ID"
+				+ " AND ER.Company = C.Company"
+				+ " AND P.Status = True"
+				+ " AND EE.Skills LIKE CONCAT('%;', P.Position_Title ,';%')"
+				+ " AND P.Salary >= EE.Expected_Salary "
+				+ " AND EE.Experience >= P.Experience";
 		
 		try {
 			Statement stmt = con.createStatement();
 			ResultSet rs = stmt.executeQuery(qry); 
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		
-		/* Report results */
+			/* Report results */
+			System.out.print("Your available positions are:\n"
+					+ "Position_ID, Position_Title, Salary, Company, Size, Founded\n");
+			while(rs.next()) {
+				System.out.print(rs.getString("Position_ID")+", ");
+				System.out.print(rs.getString("Position_Title")+", ");
+				System.out.print(rs.getString("Salary")+", ");
+				System.out.print(rs.getString("Company")+", ");
+				System.out.print(rs.getString("Size")+", ");
+				System.out.print(rs.getString("Founded")+"\n");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
 	}
 
 }
